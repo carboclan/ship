c0cf16f2b278d71293374fe85ffb6a970bcd0304
Jenil Thakker <jenilkthakker@gmail.com>
2020-05-20 00:16:23 +0530

    adds web3

---
+++ frontend/web3.js
@@ -0,0 +1,9 @@
+import Web3 from 'web3';
+
+const provider = (typeof window.web3 !== 'undefined') ?
+    window.web3.currentProvider :
+    new Web3.providers.HttpProvider("http://localhost:8545");
+
+const web3 = new Web3(provider);
+
+export default web3;
