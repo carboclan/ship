ac1792a5d88a6d96f4cd37c71bf6373af3957d89
Jenil Thakker <jenilkthakker@gmail.com>
2020-05-22 23:43:22 +0530

    adds ocDAI, and optionsFactory abi, deployend address on rinkeby

--- frontend/src/utils/addresses/address.js
+++
@@ -1,8 +0,0 @@
-// Rinkeby
-let address = 
-{
-    'ocDAI': '0x57cC8708eFEB7f7D42E4d73ab9120BC275f1DB59',
-    'optionsFactory': '0x34Da8b34c82988e7FF8F98CA35963057fC0ec9bb'
-};
-
-export default address;
