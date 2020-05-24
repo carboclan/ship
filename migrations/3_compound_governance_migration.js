1d8bee75af84d6149ef8fc7964d86056a5e0690e
Chuqiao Ren <rchuqiao@protonmail.com>
2020-05-24 14:23:24 -0700

    Revert kovan

--- migrations/3_compound_governance_migration.js
+++ migrations/3_compound_governance_migration.js
@@ -2,12 +2,12 @@ var Comp = artifacts.require("Comp");
 var GovernorAlpha = artifacts.require("GovernorAlpha");
 async function doDeploy(deployer, network, accounts) {
     // Kovan account
-    // await deployer.deploy(Comp, '0x5224abe5757eCe7c9BE1bc72102Ed1E7F02D8003');
+    await deployer.deploy(Comp, '0x5224abe5757eCe7c9BE1bc72102Ed1E7F02D8003');
     // Guardian address is my address for now on Kovan
-    // await deployer.deploy(GovernorAlpha, '0x5fE368A673309534b7801b404a2A4e0dD488c67A', Comp.address, '0x5224abe5757eCe7c9BE1bc72102Ed1E7F02D8003')
+    await deployer.deploy(GovernorAlpha, '0x5fE368A673309534b7801b404a2A4e0dD488c67A', Comp.address, '0x5224abe5757eCe7c9BE1bc72102Ed1E7F02D8003')
 
     // Rinkeby
-    await deployer.deploy(Comp, )
+    // await deployer.deploy(Comp, )
     
 }
 // module.exports = function(deployer, network, accounts) {
