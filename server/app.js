001a9f54128f6fbd0b3eb35550b9cb94f2a9454a
Jenil Thakker <jenilkthakker@gmail.com>
2020-05-28 00:04:22 +0530

    adds deployment script to heroku

--- server/app.js
+++ server/app.js
@@ -39,4 +39,12 @@ app.use('/api/projects', projects);
 
 const port = process.env.PORT || 3000; 
 
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('frontend/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
+
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
